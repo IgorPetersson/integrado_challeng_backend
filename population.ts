dd7fcfa7f1fcab37d85ef1dc5adb1e1af3d6a389
igor <igorpeterssontrab@gmail.com>
2022-03-08 14:33:49 -0400

    feat: added script to populate the bank

---
+++ population.ts
@@ -0,0 +1,47 @@
+import axios from "axios"
+
+import mongoose from "mongoose"
+import University from "./src/models/university"
+
+const population = async () => {
+
+        mongoose.connect('mongodb://localhost/integrado_database')
+        const countrys = [
+            "argentina",
+            "brazil",
+            "chile",
+            "colombia",
+            "paraguai",
+            "peru",
+            "suriname",
+            "uruguay"
+        ]
+    
+        const api = axios.create(
+            {
+                baseURL: "http://universities.hipolabs.com"
+            }
+        )
+        
+        const universities  = []
+
+        for(let i = 0; i < countrys.length; i++){
+            const universitiesCountry = await api.get(`/search?country=${countrys[i]}`)
+            universities.push(universitiesCountry.data)
+        }
+
+        for(let i = 0; i < universities.length; i++){
+            for(let j = 0; j < universities[i].length; j++){
+                const university = await University.findOne({name: universities[i][j].name, alpha_two_code: universities[i][j].alpha_two_code, country: universities[i][j].country})
+                
+                if(!university){
+                    await University.create(universities[i][j])
+                }
+            }
+        }
+
+        mongoose.disconnect()        
+        
+}  
+    
+population()
