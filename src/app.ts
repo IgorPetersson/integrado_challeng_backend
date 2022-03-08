9078b3bb0103592dea14f676f91185f50d3b0f1b
igor <igorpeterssontrab@gmail.com>
2022-03-08 14:14:27 -0400

    feat: Added route for error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from "express"
+import express, {Request, Response, NextFunction} from "express"
+import { handleError } from "./utils/error"
 
 const app = express()
 
 app.use(express.json())
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    handleError(err, res)
+})
 
 export default app
